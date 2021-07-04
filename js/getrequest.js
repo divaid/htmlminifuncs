3730b5d77ce98d95736b9159f3664c48b8caa87b
cuiweixing <m15001105461@163.com>
2021-07-04 20:49:20 +0800

    添加get与post单独请求文件

---
+++ js/getrequest.js
@@ -0,0 +1,27 @@
+function getRequest(url,param, sucCallback, failCallback)
+{
+    let xmlHttp;
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlHttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlHttp.onreadystatechange=function()
+    {
+        if (xmlHttp.readyState==4 && xmlHttp.status==200)
+        {
+            if (sucCallback) {
+                sucCallback(xmlHttp.responseText)
+            }
+        } else {
+            if (failCallback) {
+                failCallback(xmlHttp.responseText)
+            }
+        }
+    };
+    xmlHttp.open("GET",url+"?" +param,true);
+    xmlHttp.send();
+}
---
+++ js/postrequest.js
@@ -0,0 +1,28 @@
+
+function postRequest(url,param, sucCallback, failCallback)
+{
+    let xmlHttp;
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlHttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlHttp.onreadystatechange=function()
+    {
+        if (xmlHttp.readyState==4 && xmlHttp.status==200)
+        {
+            if (sucCallback) {
+                sucCallback(xmlHttp.responseText)
+            }
+        } else {
+            if (failCallback) {
+                failCallback(xmlHttp.responseText)
+            }
+        }
+    };
+    xmlHttp.open("POST",url,true);
+    xmlHttp.send(param);
+}
